9d2480c0baa88b87d29116a1eb83d664298e15b5
Heverton-prog <hevertonsosilva@gmail.com>
2024-10-05 21:03:50 -0300

    Desafio da classificação do herói, primeiro desafio

---
+++ index1.js
@@ -0,0 +1,20 @@
+let heroi = "Heverton"
+let xp = 1001
+
+if (xp <= 1000 ){//ferro
+    console.log("Aqui é apenas o começo, " + heroi + ", você começa no nível ferro!")
+}else if (xp >= 1001 && xp <= 2000){//bronze
+    console.log("Olá, meu caro aventureiro, " + heroi + ", seu nível atual é bronze!")
+}else if (xp >= 2001 && xp <= 5000){//prata
+    console.log("Vejo que avançou um pouco, " + heroi + ", seu nivel atual é prata!")
+}else if (xp >= 5001 && xp <= 7000){//ouro
+    console.log("Meus parabéns, " + heroi + ", seu nivel atual é ouro!")
+}else if (xp >= 7001 && xp <= 8000){//platina
+    console.log("Incrível, " + heroi + ", seu nível atual é platina!")
+}else if (xp >= 8001 && xp <= 9000){//ascendente
+    console.log("Você está mais forte, " + heroi + ", seu nível atual é ascendente!")
+}else if (xp >= 9001 && xp <= 10000){//imortal
+    console.log("Quase um Deus, " + heroi + ", seu nível atual é imortal!")
+}else if (xp >= 10001){//Radiante
+    console.log("Você está no pico, " + heroi + ", seu nível atual é radiante!")
+}
